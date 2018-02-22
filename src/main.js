9b2ba061ea2b366698bac87e3d9f90b4d03e1faa
Hiroshi MITANI <hmitani0429@gmail.com>
2018-02-22 23:48:31 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI, {
+  size: 'small',
+  locale
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
