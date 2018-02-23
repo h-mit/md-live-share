c224ff1047fa5dd0d2192c97516a61fb2e3c3e0e
Hiroshi MITANI <hmitani0429@gmail.com>
2018-02-23 23:50:29 +0900

    MarkdownEditorコンポーネントを追加

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+<template lang="pug">
+#app
+  el-container
+    el-header
+      span.title Markdown Live Share
+    el-container
+      el-main
+        MarkdownEditor
+      el-aside(width="150px")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MarkdownEditor from './components/MarkdownEditor'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MarkdownEditor
   }
 }
 </script>
 
 <style>
+html, body, #app, .el-container {
+  margin: 0;
+  height: 100%;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.el-header {
+  background-color: #3F51B5;
+  color: #eee;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #bbb;
+  color: #333;
+}
+
+.el-main {
+  padding: 0;
+}
+
+.title {
+  font-family: 'Poiret One', cursive;
+  font-size: 20px;
+  font-weight: bold;
+  letter-spacing: .2em;
 }
 </style>
---
+++ src/components/MarkdownEditor.vue
@@ -0,0 +1,76 @@
+<template lang="pug">
+.markdown-editor
+  textarea.editor(
+    :value="mdText"
+    @input="updateText"
+    spellcheck="false"
+  )
+  div.preview.markdown-body(
+    v-html="previewText"
+  )
+</template>
+
+<script>
+import _ from 'lodash'
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'github-markdown-css/github-markdown.css'
+import 'highlight.js/styles/atom-one-dark.css'
+
+marked.setOptions({
+  breaks: true,
+  sanitize: true,
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value
+  }
+})
+
+export default {
+  name: 'MarkdownEditor',
+  data () {
+    return {
+      mdText: '# sample\n'
+    }
+  },
+  computed: {
+    previewText () {
+      return marked(this.mdText)
+    }
+  },
+  methods: {
+    updateText: _.debounce(function (e) {
+      console.log(e.target.value)
+      this.mdText = e.target.value
+    }, 300)
+  }
+}
+</script>
+
+<style scoped>
+.markdown-editor {
+  display: flex;
+  height: 100%;
+}
+
+.editor {
+  flex: 1;
+  padding: 15px;
+  border: none;
+  border-right: 1px solid #ccc;
+  background: #282c34;
+  color: #ccc;
+  resize: none;
+  outline: none;
+  font-family: 'MyricaM M', 'Ricty Diminished', Consolas, 'Courier New', Courier, Monaco, monospace;
+  font-size: 15px;
+  line-height: 1.4;
+}
+
+.preview {
+  flex: 1;
+  padding: 15px;
+  font-family: 'Hiragino Kaku Gothic ProN', 'ヒラギノ角ゴ ProN W3', Meiryo, メイリオ, Osaka, 'MS PGothic', arial, helvetica, sans-serif;
+  font-size: .9em;
+  overflow: auto;
+}
+</style>
